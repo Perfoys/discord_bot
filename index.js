a2d118cf3edf838decb626f426e66f28346e500f
Perfoys <prorok43battle@gmail.com>
2021-10-30 17:20:30 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const TOKEN = OTAzOTc4OTExOTA4NjI2NDcz.YX019w.K-GeM7_abt4s96Tu3W_bWagc1cY;
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
@@ -13,4 +11,4 @@ client.on('message', (msg) => {
   }
 })
 
-client.login(TOKEN)
+client.login(process.env.TOKEN)
